a0d8aeecf50a06420090fc991cdf28deb021e19b
LeBenjos <benjamin.schinkel01@outlook.fr>
2026-04-09 17:06:37 +0200

    feat: sky

--- src/experiences/views/threes/worlds/WorldThreeView.ts
+++ src/experiences/views/threes/worlds/WorldThreeView.ts
@@ -2,6 +2,7 @@ import { ViewId } from '../../../constants/experiences/ViewId';
 import ThreeViewBase from '../bases/ThreeViewBase';
 import Dunes from './components/Dunes';
 import Environment from './components/Environment';
+import Sky from './components/Sky';
 
 export default class WorldThreeView extends ThreeViewBase {
     constructor() {
@@ -16,6 +17,7 @@ export default class WorldThreeView extends ThreeViewBase {
         // this._actors.push(new TemplateModel());
         // this._actors.push(new TemplateFont());
 
+        this._actors.push(new Sky());
         this._actors.push(new Dunes());
 
         for (const actor of this._actors) this.add(actor);
