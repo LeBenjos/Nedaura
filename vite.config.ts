e9743f72eb159cb9e7ee7cb3ac334a15da9f9973
LeBenjos <benjamin.schinkel01@outlook.fr>
2026-04-27 12:02:49 +0200

    feat: basicSsl ofr https

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,18 @@
 import vue from '@vitejs/plugin-vue';
 import { defineConfig } from 'vite';
 import glsl from 'vite-plugin-glsl';
+import basicSsl from '@vitejs/plugin-basic-ssl';
 
 export default defineConfig({
     base: './',
     server: {
         host: true,
+        https: {},
     },
     build: {
         outDir: './dist',
         emptyOutDir: true,
         sourcemap: true,
     },
-    plugins: [vue(), glsl()],
+    plugins: [vue(), glsl(), basicSsl()],
 });
