b4e70d00d338c148f9e28626e350ea7422a19aa0
Tom thierry <tomthierry73@gmail.com>
2026-04-28 16:53:25 +0200

    wip

--- src/experiences/constants/experiences/Object3dId.ts
+++ src/experiences/constants/experiences/Object3dId.ts
@@ -1,7 +1,7 @@
 export const Object3DId = {
     DESERT: "desert",
     DESERT_DUNES: "dunes",
-    STATUE: "statue"
+    STATUE: "Statue"
 } as const;
 
 export type Object3DId = (typeof Object3DId)[keyof typeof Object3DId];
