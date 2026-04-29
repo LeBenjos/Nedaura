c1b419b96b6bfc263f796cbd67b3b0148d449bb3
Tom thierry <tomthierry73@gmail.com>
2026-04-29 15:52:06 +0200

    fix: duplicate imports

--- src/experiences/views/threes/worlds/components/WindLines.ts
+++ src/experiences/views/threes/worlds/components/WindLines.ts
@@ -94,6 +94,14 @@ export default class WindLines extends ThreeActorBase {
         folder.add(this._settings, 'handSpread', 0, 10, 0.01).name('handSpread');
         folder.add(this._settings, 'smoothing', 0.01, 0.5, 0.01).name('smoothing');
 
+        folder.add(this._settings, 'enabled').name('enabled');
+        folder.add(this._settings, 'handDepth', -10, 10, 0.01).name('handDepth');
+        folder.add(this._settings, 'handSpread', 0, 10, 0.01).name('handSpread');
+        folder.add(this._settings, 'smoothing', 0.01, 0.5, 0.01).name('smoothing');
+
+        const controllers: Partial<Record<keyof typeof THREE_WORLD_CONFIG.windLines, Controller>> = {};
+
+
         controllers.lineWidth = folder
             .add(this._settings, 'lineWidth', 0.01, 2, 0.01)
             .name('lineWidth')
